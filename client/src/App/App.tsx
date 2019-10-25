7699c5b85ca61a9538542b0d3fc82590f734791f
Joe Boylson <joeboylson@gmail.com>
2019-10-25 14:00:34 -0400

    adding project app configurations

---
+++ client/src/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <iframe src={'/assignment1_introduction'}></iframe>
+    </div>
+  );
+}
+
+export default App;
