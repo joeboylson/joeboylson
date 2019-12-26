d5dd3074eb05738deedd54bbae1f0493b1c14b04
Joe Boylson <joeboylson@gmail.com>
2019-12-26 16:29:03 -0500

    enhancing nav

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -21,42 +21,43 @@ const Nav: React.FC<NavProps> = (Props) => {
   return (
     <div id={'nav'}
       onClick={() => setNavIsOpen(!navIsOpen)}
+      className={`${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`}
     >
+
+      <div className={`menu-icon ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`}>
+        <Icon className={`transparent ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'menu'} />
+      </div>
 
       <div className={`nav-links ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`}>
 
-        { navLinks.map((link: any, index: number) => {
-          return <h3 
+        {navLinks.map((link: any, index: number) => {
+          return <h3
             key={index}
             className={`nav-link ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`}
-            onClick={() => Props.setRoute(link.route)}  
+            onClick={() => Props.setRoute(link.route)}
           >
-            { link.name }
+            {link.name}
           </h3>
         })}
 
         <div className={'nav-icons'}>
-          <Icon 
-            className={`blue ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'github'} 
+          <Icon
+            className={`blue ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'github'}
             onClick={() => openNewTab('https://github.com/')}
           />
 
-          <Icon 
-            className={`green ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'linkedin'} 
+          <Icon
+            className={`green ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'linkedin'}
             onClick={() => openNewTab('https://www.linkedin.com/in/joe-boylson-34603b139/')}
           />
 
-          <Icon 
-            className={`yellow ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'instagram'} 
+          <Icon
+            className={`yellow ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'instagram'}
             onClick={() => openNewTab('https://www.instagram.com/jobo.jpg/')}
           />
 
         </div>
 
-      </div>
-
-      <div className={`menu-icon ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`}>
-        <Icon className={`transparent ${navIsOpen ? 'nav-is-open' : 'nav-is-closed'}`} type={'menu'} />
       </div>
 
       <div className={'nav-caption'}>
---
+++ resources.md
@@ -0,0 +1,31 @@
+# SIMPLE EXPRESS APP WITH INDEX.HTML
+- https://expressjs.com/en/starter/hello-world.html
+
+# EXPRESS GENERATOR
+- https://expressjs.com/en/starter/generator.html
+
+
+# HEROKU HOSTING
+- https://dev.to/smithmanny/deploy-your-react-app-to-heroku-2b6f
+  - at the bottom is github integration instructions (it's easier)
+
+# REACT DOCS
+- https://reactjs.org/docs/hello-world.html
+  - paging through "Main Concepts" will give a great explanation of React
+
+
+# GENERAL REACT STUFF
+- understand the difference between functional and class components
+  - FUNCTIONAL components are ideal
+- React Hooks:
+  - useState() (***you'll use this a lot)
+  - useEffect()
+
+
+# STEAL MY APP
+- https://github.com/joeboylson/joeboylson
+- use commands:
+  - `git clone https://github.com/joeboylson/joeboylson.git`
+  - `npm install`
+  - `cd client` and then `npm install`
+  - from top folder run `yarn dev`
