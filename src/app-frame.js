d985499310f84367e158f9dc9e78714c0005b6c8
Joe Boylson <jboylson@saec-kv.com>
2020-05-14 22:25:32 -0400

    created coordinate routing system; using pubsub-js

--- src/app-frame.js
+++ src/app-frame.js
@@ -1,32 +1,56 @@
 import {LitElement, html} from 'lit-element';
+import Router from './utils/router';
 
 class AppFrame extends LitElement {
     static get properties() {
         return {
-            x: {type: Number}
+            currentPage: {type: Object},
+            nextPages: {type: Object}
         };
     }
 
     constructor() {
         super();
+        this.currentPage = null;
+        this.nextPages = null;
 
-        this.x = 0;
+        PubSub.subscribe('ROUTE_TO', (_, data) => this.setPage(data));
     }
 
-    increment () {
-        this.x = this.x + 1;
+    firstUpdated() {
+        this.currentPage = Router.findPage(Router.x, Router.y);
+        this.nextPages = this.currentPage.getNextPages();
+    }
+
+    setPage (page) {
+        this.currentPage = page
+        this.nextPages = this.currentPage.getNextPages();
     }
 
     render() {
+
         return html `
             <style>
+                @import "https://cdn.lineicons.com/2.0/LineIcons.css";
+
                 p {
                     color: teal;
                 }
             </style>
 
-            <p>POLYMER: ${this.x}</p>
-            <button @click="${this.increment}">+</button>
+            <p>POLYMER: X: ${Router.x} | Y: ${Router.y}</p>
+
+            ${this.currentPage ? this.currentPage.component : ""}
+
+            ${this.nextPages ? html`
+                <div>
+                    ${this.nextPages.top ? html`<button @click="${() => Router.routeTo(this.nextPages.top.x, this.nextPages.top.y)}">UP: ${this.nextPages.top.name}</button>` : null}
+                    ${this.nextPages.right ? html`<button @click="${() => Router.routeTo(this.nextPages.right.x, this.nextPages.right.y)}">RIGHT: ${this.nextPages.right.name}</button>` : null}
+                    ${this.nextPages.bottom ? html`<button @click="${() => Router.routeTo(this.nextPages.bottom.x, this.nextPages.bottom.y)}">BOTTOM: ${this.nextPages.bottom.name}</button>` : null}
+                    ${this.nextPages.left ? html`<button @click="${() => Router.routeTo(this.nextPages.left.x, this.nextPages.left.y)}">LEFT: ${this.nextPages.left.name}</button>` : null}
+
+                </div>    
+            ` : html`no next pages`}
         `
     }
 }
---
+++ src/utils/router.js
@@ -0,0 +1,82 @@
+import { html } from 'lit-element';
+
+function getNextPages () {
+    return {
+        top: Router.findPage(this.x, this.y - 1),
+        right: Router.findPage(this.x + 1, this.y),
+        bottom: Router.findPage(this.x, this.y + 1),
+        left: Router.findPage(this.x - 1, this.y)
+    }
+}
+
+const Router = {
+    
+    x: 1,
+    y: 0,
+
+    pages: [{
+            name: 'Index',
+            component: html`<p>Index</p>`,
+            x: 1,
+            y: 0,
+            getNextPages
+        }, {
+            name: 'About / Personal',
+            component: html`<p>About / Personal</p>`,
+            x: 0,
+            y: 1,
+            getNextPages
+        }, {
+            name: 'About / Professional',
+            component: html`<p>About / Professional</p>`,
+            x: 1,
+            y: 1,
+            getNextPages
+        }, {
+            name: 'About / Site',
+            component: html`<p>About / Site</p>`,
+            x: 2,
+            y: 1,
+            getNextPages
+        }, {
+            name: 'Projects',
+            component: html`<p>Projects</p>`,
+            x: 1,
+            y: 2,
+            getNextPages
+        }, {
+            name: 'Contact / Me',
+            component: html`<p>name: 'Contact / Me',</p>`,
+            x: 1,
+            y: 3,
+            getNextPages
+        }, {
+            name: 'Contact / Questionaire',
+            component: html`<p>name: 'Contact / Questionaire',</p>`,
+            x: 2,
+            y: 3,
+            getNextPages
+        }
+    ],
+
+    findPage: (x, y) => {
+        return Router.pages.find(page => {
+            if (page.x === x && page.y === y) return page;
+        })
+    },
+
+    routeTo: (x, y) => {
+
+        let nextPage = Router.findPage(x, y);
+
+        if (nextPage) {
+            Router.x = nextPage.x;
+            Router.y = nextPage.y;
+
+            PubSub.publish('ROUTE_TO', nextPage)
+        }
+
+    }
+}
+
+export default Router;
